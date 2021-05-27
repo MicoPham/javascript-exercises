fa317bcba7aa75eb02c6b8ffc206de9a03a6ba8d
MicoPham <michaelxpham@gmail.com>
2021-05-26 22:27:35 -0700

    Make tempConversion.js pass Jest spec

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
 const ftoc = function() {
-
+  var result = ((arguments[0] - 32) * (5/9));
+  return Math.round(result * 10) / 10;
 };
 
 const ctof = function() {
-
+  var result = (arguments[0] * (9/5) + 32);
+  return Math.round(result * 10) / 10;
 };
 
 module.exports = {
