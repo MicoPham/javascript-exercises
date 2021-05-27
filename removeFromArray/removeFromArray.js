23fb571d813d2e06b6ba2d36f59ca2ff3b2b8762
MicoPham <michaelxpham@gmail.com>
2021-05-26 21:19:56 -0700

    Make removeFromArray pass Jest spec

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(...args) {
+    var arr = arguments[0];
+    var result = [];
+    for (var i = 0; i < arr.length; i++) {
+        if (!args.includes(arr[i])) {
+            result.push(arr[i]);
+        }
+    }
+    return result;
 };
 
 module.exports = removeFromArray;
