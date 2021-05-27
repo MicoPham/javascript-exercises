fead3127ff018a70d619bda56c6bb8a6dec54e59
MicoPham <michaelxpham@gmail.com>
2021-05-26 21:52:35 -0700

    Make sumAll.js pass Jest spec

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,21 @@
 const sumAll = function() {
+    if ((arguments[0] < 0) || (arguments[1] < 0) || (typeof(arguments[0]) !== "number") || (typeof(arguments[1]) !== "number")) {
+        return "ERROR";
+    }
 
+
+    var sum = 0;
+    if (arguments[0] < arguments[1]) {
+        for (var i = arguments[0]; i <= arguments[1]; i++) {
+        sum += i;
+    }
+    return sum;
+    } else {
+        for (var i = arguments[0]; i >= arguments[1]; i--) {
+            sum += i;
+    }
+    return sum;
 };
+}
 
 module.exports = sumAll;
