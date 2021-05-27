05171f1ba48929c5dd0d0a6e00e6d217aacc5d14
MicoPham <michaelxpham@gmail.com>
2021-05-26 19:49:47 -0700

    Make reverseString.js pass Jest testing framework

--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(string) {
+  var arr = string.split("");
+  var reverseArr = [];
 
+  for (i = 0; i < arr.length; i++) {
+    reverseArr.unshift(arr[i]);
+  }
+  return reverseArr.join("");
 };
 
 module.exports = reverseString;
