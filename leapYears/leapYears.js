1011e298963cd77106f70ce24d3079884e893134
MicoPham <michaelxpham@gmail.com>
2021-05-26 22:11:22 -0700

    Make leapYears.js pass Jest spec

--- leapYears/leapYears.js
+++ leapYears/leapYears.js
@@ -1,5 +1,9 @@
 const leapYears = function() {
-
+    if ((arguments[0] % 4 === 0) && (arguments[0] % 100 !== 0) || ((arguments[0] % 400 === 0))) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 module.exports = leapYears;
