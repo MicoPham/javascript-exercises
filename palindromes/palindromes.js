96c81cddc3f994753273412113ae8fa71c2d524d
MicoPham <michaelxpham@gmail.com>
2021-06-15 16:54:29 -0700

    Make palindromes.js pass spec

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,5 +1,10 @@
 const palindromes = function () {
-
+    let filtered = arguments[0].toLowerCase().replace(/[^A-Za-z]/g, '');
+    let newStr = filtered.split('');
+    newStr = newStr.reverse();
+    newStr = newStr.join('');
+    
+    return (newStr === filtered);
 };
 
 module.exports = palindromes;
