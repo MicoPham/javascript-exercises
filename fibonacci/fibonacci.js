b656c1c4e912cb51289cba537e674c70960961ec
MicoPham <michaelxpham@gmail.com>
2021-06-15 17:47:35 -0700

    Make fibonacci.js pass spec

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,5 +1,21 @@
 const fibonacci = function() {
+    if (arguments[0] < 0) {
+        return "OOPS";
+    }
 
+    if (arguments[0] === 0) {
+        return 0;
+    }
+
+    let previous = 0;
+    let current = 1;
+
+    for (let i = 1; i < arguments[0]; i++) {
+        const next = previous + current;
+        previous = current;
+        current = next;
+    }
+    return current;
 };
 
 module.exports = fibonacci;
