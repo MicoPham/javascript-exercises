c4a112cbb5eef6cca65615bf501e9306fc67a9ee
MicoPham <michaelxpham@gmail.com>
2021-06-15 19:50:10 -0700

    Make findTheOldest.js pass spec

--- findTheOldest/findTheOldest.js
+++ findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
 const findTheOldest = function() {
+    return arguments[0].reduce((oldest, current) => {
+        let oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+        let currentAge = getAge(current.yearOfBirth, current.yearOfDeath);
+        return oldestAge > currentAge ? oldest : current;
+    })
+};
 
+function getAge(birth, death) {
+    if (!death) {
+      death = new Date().getFullYear();
+    }
+return death - birth;
 };
 
 module.exports = findTheOldest;
