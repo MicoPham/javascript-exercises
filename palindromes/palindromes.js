08cf20e8eaceac3909203df6f1190af9d48e7b79
MicoPham <michaelxpham@gmail.com>
2021-06-15 21:18:27 -0700

    Clean up palindromes.js

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,10 +1,6 @@
 const palindromes = function () {
     let filtered = arguments[0].toLowerCase().replace(/[^A-Za-z]/g, '');
-    let newStr = filtered.split('');
-    newStr = newStr.reverse();
-    newStr = newStr.join('');
-    
-    return (newStr === filtered);
+    return filtered === filtered.split('').reverse().join('');
 };
 
 module.exports = palindromes;
